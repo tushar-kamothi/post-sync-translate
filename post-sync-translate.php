db5b5a164b065252177aed3e8d13583f992c2dee
tusharkamothi <tushar.kamothi99@gmail.com>
2026-02-18 14:16:30 +0530

    Add plugin bootstrap, constants, and module loader

--- post-sync-translate.php
+++ post-sync-translate.php
@@ -6,35 +6,30 @@
  * Author: Tushar Kamothi
  */
 
-if (!defined('ABSPATH')) {
-    exit;
-}
+if (!defined('ABSPATH')) exit;
 
 define('PST_VERSION', '1.0.0');
 define('PST_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('PST_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PST_LOG_TABLE', $GLOBALS['wpdb']->prefix . 'pst_logs');
 
-class PST_Plugin {
+require_once PST_PLUGIN_DIR . 'includes/class-settings.php';
+require_once PST_PLUGIN_DIR . 'includes/class-auth.php';
+require_once PST_PLUGIN_DIR . 'includes/class-logger.php';
+require_once PST_PLUGIN_DIR . 'includes/class-rest.php';
+require_once PST_PLUGIN_DIR . 'includes/class-host.php';
+require_once PST_PLUGIN_DIR . 'includes/class-target.php';
+require_once PST_PLUGIN_DIR . 'includes/class-translate.php';
 
-    public function __construct() {
-        add_action('plugins_loaded', [$this, 'init']);
-    }
+register_activation_hook(__FILE__, ['PST_Logger', 'create_table']);
 
-    public function init() {
+add_action('plugins_loaded', function(){
 
-        require_once PST_PLUGIN_DIR . 'includes/class-settings.php';
-        require_once PST_PLUGIN_DIR . 'includes/class-rest.php';
-        require_once PST_PLUGIN_DIR . 'includes/class-host.php';
-        require_once PST_PLUGIN_DIR . 'includes/class-target.php';
-        require_once PST_PLUGIN_DIR . 'includes/class-auth.php';
-        require_once PST_PLUGIN_DIR . 'includes/class-translate.php';
-        require_once PST_PLUGIN_DIR . 'includes/class-logger.php';
+    PST_Settings::init();
+    PST_REST::init();
 
-        PST_Settings::init();
-        PST_REST::init();
+    if (get_option('pst_mode') === 'host') {
         PST_Host::init();
-        PST_Target::init();
     }
-}
 
-new PST_Plugin();
+});
